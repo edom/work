732d04405724f860f4a38b047baf4a9cd9362f7f
Erik Dominikus <software@spacetimecat.com>
2015-05-02 22:48:54 +0700

    Genericize Procedural

--- sound/src/Sound/Procedural.hs
+++ sound/src/Sound/Procedural.hs
@@ -1,16 +1,10 @@
-{-# LANGUAGE Safe #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Sound.Procedural
 (
     -- * Construction
-    gdef
-    , gconst
-    , cons
-    -- * Mapping
-    , map
-    -- * Combination
-    , zip2
+    consDouble
     -- * Integration
     , int
     -- * Array
@@ -22,45 +16,47 @@ module Sound.Procedural
     , example
     -- * Types
     , G
-    , mkG
 )
 where
 
 import Control.Applicative
-import Prelude hiding (map)
+import Foreign
 
 import qualified Data.Array.IO.Safe as AI
 import qualified Data.Array.MArray.Safe as AM
 
-type G m a = m a
+import Sound.Class
+import Sound.Buffer
+
+newtype G m a = MkG (m a)
+
+instance (Point m) => Point (G m) where point = mkG . point
+instance (Functor m) => Functor (G m) where fmap f g = mkG $ fmap f $ _step g
+instance Consume (G IO)
+instance Fill (G IO)
+
+instance (Monad m) => DeconsM m (G m) where
+    deconsM x f = do
+        h <- _step x
+        f h x
+
+instance (Applicative m) => Applicative (G m) where
+    pure = mkG . pure
+    mf <*> mx = mkG $ _step mf <*> _step mx
+
+instance (Monad m) => Monad (G m) where
+    return = mkG . return
+    m >>= k = mkG $ _step m >>= _step . k
+
+instance (Applicative m) => Zip2 (G m) where
+    zip2 f x y = mkG $ f <$> _step x <*> _step y
+    {-# INLINE zip2 #-}
 
 mkG :: m a -> G m a
-mkG = id
+mkG = MkG
 
 _step :: G m a -> m a
-_step = id
-
-gdef :: (Applicative m) => a -> G m a
-gdef = mkG . pure
-
-gconst :: (Applicative m) => a -> G m a
-gconst = gdef
-
-run :: (AM.MArray a e m, Monad m) => a Int e -> Int -> G m e -> m ()
-run a !n g = loop 0
-    where
-        step = _step g
-        loop !i
-            | i >= n = return ()
-            | otherwise = do
-                x <- step
-                AM.writeArray a i x
-                loop (i + 1)
-
-fill :: (AM.MArray a e m, Functor m, Monad m) => a Int e -> G m e -> m ()
-fill a !g = do
-    !n <- AM.rangeSize <$> AM.getBounds a
-    run a n g
+_step (MkG x) = x
 
 {- $array
 See also:
@@ -82,9 +78,6 @@ newIntArray n = AM.newArray (0, n - 1) 0
 withDoubleArray :: Int -> (AI.IOUArray Int Double -> IO a) -> IO a
 withDoubleArray n k = newDoubleArray n >>= k
 
-map :: (Functor m) => (a -> b) -> G m a -> G m b
-map f g = mkG $ fmap f $ _step g
-
 int :: Double -> G IO Double -> IO (G IO Double)
 int !dt !g = do
     !s <- newDoubleArray 1
@@ -97,29 +90,27 @@ int !dt !g = do
         stepg = _step g
 {-# INLINE int #-}
 
-cons :: Double -> G IO Double -> IO (G IO Double)
-cons !h !g = do
+consDouble :: Double -> G IO Double -> IO (G IO Double)
+consDouble !h !g = do
     !s <- newIntArray 1
-    return $ do
+    return . mkG $ do
         !b <- AM.readArray s 0
         case b of
             0 -> do
                 AM.writeArray s 0 1
                 return h
-            _ -> g
-{-# INLINE cons #-}
-
-zip2 :: (Applicative m) => (a -> b -> c) -> G m a -> G m b -> G m c
-zip2 f x y = mkG $ f <$> _step x <*> _step y
+            _ -> _step g
+{-# INLINE consDouble #-}
 
 example :: IO ()
 example = do
-    !a <- newDoubleArray 1024
-    !g <- int dt (gdef 1) >>= int dt >>= cons 123 >>= int dt
-    -- !g <- int dt (gdef 1) >>= cons 123
-    fill a g
-    !x <- AM.readArray a 0
-    print x
+    allocaBuffer 1024 $ \ a -> do
+        !g <- int dt (point 1) >>= int dt >>= consDouble 123 >>= int dt
+        -- let !g = point 123 :: G IO Double
+        -- !g <- int dt (point 1) >>= consDouble 123
+        fill a g >> return ()
+        !x <- peekElemOff (bufPtr a) 0
+        print x
     where
         dt :: Double
         dt = recip 44100
