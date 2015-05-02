d92edaa88f4cd1bdefe4fd36fa5f72e1b3d0889d
Erik Dominikus <software@spacetimecat.com>
2015-05-03 02:37:41 +0700

    Genericize sample

--- sound/src/Sound/Sample.hs
+++ sound/src/Sound/Sample.hs
@@ -1,18 +1,17 @@
 {-# LANGUAGE BangPatterns #-}
 module Sound.Sample
 (
-    lsample
+    sample
 )
 where
 
+import Sound.Class
 import Sound.Hint
-import Sound.InfList
 
 {- |
 Evaluate a function at each time
 that is an integral multiple of the sampling period.
 -}
-lsample :: (Num t) => StepSize t -> (t -> a) -> L a
-lsample !p !f =
-    lunfoldr (p +) f 0
-{-# INLINE lsample #-}
+sample :: (Unfold f, Num t) => StepSize t -> (t -> a) -> f a
+sample p f = unfold f (p +) 0
+{-# INLINE sample #-}
