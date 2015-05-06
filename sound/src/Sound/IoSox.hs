cb141463f2fcadc74d09234fbba89c8b4720e167
Erik Dominikus <software@spacetimecat.com>
2015-05-06 23:51:00 +0700

    Add IoSox

--- sound/src/Sound/IoSox.hs
+++ sound/src/Sound/IoSox.hs
@@ -1,8 +1,14 @@
 module Sound.IoSox
 (
+    createSoxProcess
+    , independentSox
     -- * Input from file
-    rvreadfilewav
+    , rvreadfilewav
     , rlreadfilewav
+    , SoxReadFile(..)
+    -- * Formats
+    , Wav(..)
+    , Snd(..)
     -- * Format conversion
     , conwavau
 )
@@ -38,13 +44,7 @@ sudo apt-get install sox
 -}
 conwavau :: Bs.ByteString -> IO Bs.ByteString
 conwavau wav_ = do
-    (Just i, Just o, _, p) <- P.createProcess (P.proc sox args)
-        {
-            P.cwd = Just "/tmp"
-            , P.std_in = P.CreatePipe
-            , P.std_out = P.CreatePipe
-            , P.std_err = P.Inherit
-        }
+    (i, o, p) <- createSoxProcess args
     let
         loop wav au = do
             wavTail <- Bs.hPutNonBlocking i wav
@@ -56,8 +56,38 @@ conwavau wav_ = do
                 _ -> loop wavTail (Bs.append au auTail)
     loop wav_ Bs.empty
     where
-        sox = "sox"
         args = ["-", "--type", "au", "--encoding", "floating-point", "--bits", "64", "-"]
+
+createSoxProcess :: [String] -> IO (Handle, Handle, P.ProcessHandle)
+createSoxProcess args = do
+    (Just i, Just o, _, p) <- P.createProcess (P.proc sox args)
+        {
+            P.cwd = Just "/tmp"
+            , P.std_in = P.CreatePipe
+            , P.std_out = P.CreatePipe
+            , P.std_err = P.Inherit
+        }
+    return (i, o, p)
+    where
+        sox = "sox"
+
+-- | Create a child process without piping any standard streams.
+independentSox :: [String] -> IO Ex.ExitCode
+independentSox args = P.rawSystem "sox" args
+
+data Wav = Wav deriving (Read, Show)
+data Snd = Snd deriving (Read, Show)
+
+{-
+class GeneralWrite what whither monad result where
+    write :: what -> whither -> monad result
+-}
+
+class SoxReadFile format collection sample where
+    soxReadFile :: format -> FilePath -> IO (Rated (collection sample))
+
+instance SoxReadFile Wav Vu.Vector Double where
+    soxReadFile _ = rvreadfilewav
 
 rvreadfilewav :: FilePath -> IO (Rated (Vu.Vector Double))
 rvreadfilewav path =
