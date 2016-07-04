60ce06b2f2fa98243f91f1a9d1f2e96300ed98b1
Erik Dominikus <software@spacetimecat.com>
2016-07-05 01:50:36 +0700

    SqlSelectPrefix: Deprecate

--- java-fun/src/main/java/com/spacetimecat/objmap/SqlSelectPrefix.java
+++ java-fun/src/main/java/com/spacetimecat/objmap/SqlSelectPrefix.java
@@ -3,6 +3,7 @@ package com.spacetimecat.objmap;
 /**
  * <p>Each instance of this can generate the "SELECT column-list FROM table" prefix part of a SELECT statement.</p>
  */
+@Deprecated
 public interface SqlSelectPrefix
 {
     String sqlSelectPrefix ();
