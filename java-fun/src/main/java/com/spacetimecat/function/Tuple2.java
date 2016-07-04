d1aa792bbfa699b066b7cbade50dcdc7a1a13364
Erik Dominikus <software@spacetimecat.com>
2016-07-04 15:47:59 +0700

    Add Tuple2

---
+++ java-fun/src/main/java/com/spacetimecat/function/Tuple2.java
@@ -0,0 +1,13 @@
+package com.spacetimecat.function;
+
+public final class Tuple2<A, B>
+{
+    public final A a;
+    public final B b;
+
+    public Tuple2 (A a, B b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+}
