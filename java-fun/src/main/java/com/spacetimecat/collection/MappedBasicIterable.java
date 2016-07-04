a2b66ef0826d1f9b0c3be1675fba7a452f069735
Erik Dominikus <software@spacetimecat.com>
2016-07-04 20:32:34 +0700

    MappedBasicIterable: Generalize

--- java-fun/src/main/java/com/spacetimecat/collection/MappedBasicIterable.java
+++ java-fun/src/main/java/com/spacetimecat/collection/MappedBasicIterable.java
@@ -5,9 +5,9 @@ import com.spacetimecat.function.BasicFunction1;
 final class MappedBasicIterable<A, B> implements BasicIterable<B>
 {
     private final BasicIterable<A> bi;
-    private final BasicFunction1<A, B> f;
+    private final BasicFunction1<? super A, B> f;
 
-    MappedBasicIterable (BasicIterable<A> bi, BasicFunction1<A, B> f)
+    MappedBasicIterable (BasicIterable<A> bi, BasicFunction1<? super A, B> f)
     {
         this.bi = bi;
         this.f = f;
