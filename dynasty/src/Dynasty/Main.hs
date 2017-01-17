7444f9ffde5c4a902c79ca50fb862d3119f55de9
Erik Dominikus <software@spacetimecat.com>
2017-01-18 02:35:38 +0700

    Main: Extract initialize

--- dynasty/src/Dynasty/Main.hs
+++ dynasty/src/Dynasty/Main.hs
@@ -22,52 +22,44 @@ dynastyMain = do
             putStrLn "This program does not support your terminal."
         else do
             C.echo False
-            let state0 = flip S.exec S.initialState $ do
-                let
-                    irish p = p { P.culture = A.Irish }
-                    angloSaxon p = p { P.culture = A.AngloSaxon }
-                    named name p = p { P.name = name }
-                    countOf county p = p { P.titles = T.countOf county : P.titles p }
-                    dukeOf duchy p = p { P.titles = T.dukeOf duchy : P.titles p }
-                    kingOf kingdom p = p { P.titles = T.kingOf kingdom : P.titles p }
-                    catholic p = p { P.religion = R.Catholic }
-                    addTrait t p = p { P.traits = t : P.traits p }
-                    kind = addTrait U.Kind
-                    patient = addTrait U.Patient
-                    envious = addTrait U.Envious
-                    wroth = addTrait U.Wroth
-                    female p = p { P.sex = L.Female }
-                M.mapM_ S.newPersonWith $ concat
-                    [
-                        map ((irish . catholic) .) [
-                            named "Murchad" . countOf "Tuadhmhumhain" . dukeOf "Mumu"
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Urmhumhain"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Deasmhumhain"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Osraige"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Cill Dara"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Breitne"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Connachta"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Laigin"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Dubhlinn"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Ulster"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Tir Eoghain"] }
-                            , \ p -> p { P.name = "Who", P.titles = [T.countOf "Tir Chonaill"] }
-                        ]
-                        ,
-                        map ((angloSaxon . catholic) .)
-                        [
-                            named "Edgar" . kingOf "England"
-                            , named "Harold Godwinson" . countOf "Hereford" . kingOf "England"
-                        ]
-                        ,
-                        [
-                            envious . wroth . named "Noman" . countOf "Nocounty" . dukeOf "Noduchy" . kingOf "Nomanland"
-                            , kind . patient . named "Yesman" . countOf "Yescounty" . dukeOf "Yesduchy" . kingOf "Yesland"
-                            , female . named "Maywoman" . countOf "Maycounty"
-                        ]
-                    ]
+            let state0 = flip S.exec S.initialState initialize
             mainLoop window state0
             C.endWin
+
+initialize :: S.StateM ()
+initialize = do
+    M.mapM_ S.newPersonWith $ concat
+        [
+            map ((irish . catholic) .) [
+                named "Murchad" . countOf "Tuadhmhumhain" . dukeOf "Mumu"
+            ]
+            ,
+            map ((angloSaxon . catholic) .)
+            [
+                named "Edgar" . kingOf "England"
+                , named "Harold Godwinson" . countOf "Hereford" . kingOf "England"
+            ]
+            ,
+            [
+                envious . wroth . named "Noman" . countOf "Nocounty" . dukeOf "Noduchy" . kingOf "Nomanland"
+                , kind . patient . named "Yesman" . countOf "Yescounty" . dukeOf "Yesduchy" . kingOf "Yesland"
+                , female . named "Maywoman" . countOf "Maycounty"
+            ]
+        ]
+    where
+        irish p = p { P.culture = A.Irish }
+        angloSaxon p = p { P.culture = A.AngloSaxon }
+        named name p = p { P.name = name }
+        countOf county p = p { P.titles = T.countOf county : P.titles p }
+        dukeOf duchy p = p { P.titles = T.dukeOf duchy : P.titles p }
+        kingOf kingdom p = p { P.titles = T.kingOf kingdom : P.titles p }
+        catholic p = p { P.religion = R.Catholic }
+        addTrait t p = p { P.traits = t : P.traits p }
+        kind = addTrait U.Kind
+        patient = addTrait U.Patient
+        envious = addTrait U.Envious
+        wroth = addTrait U.Wroth
+        female p = p { P.sex = L.Female }
 
 mainLoop :: C.Window -> S.State -> IO ()
 mainLoop window = theRealMainLoop $ E.curses window
