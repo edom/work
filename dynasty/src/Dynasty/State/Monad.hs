1b7d5ba84ebf1b6fa14236a2df7768816a884061
Erik Dominikus <software@spacetimecat.com>
2017-01-19 02:11:17 +0700

    Add polymorphic functions in MonadState

--- dynasty/src/Dynasty/State/Monad.hs
+++ dynasty/src/Dynasty/State/Monad.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Dynasty.State.Monad
 (
+    -- * Class
+
+    MonadState(..)
+
     -- * State monad
 
-    StateM
+    , StateM
     , exec
     , eval
-    , gets
-    , modify
 
     -- * On the monad
 
@@ -24,7 +28,22 @@ import qualified Dynasty.Date as D
 import qualified Dynasty.Person as P
 import qualified Dynasty.State as S
 
+class (Monad m) => MonadState m where
+
+    get :: m S.State
+
+    modify :: (S.State -> S.State) -> m ()
+
+instance (Monad m) => MonadState (M.StateT S.State m) where
+
+    get = M.get
+
+    modify = M.modify
+
 type StateM = M.State S.State
+
+gets :: (MonadState m) => (S.State -> s) -> m s
+gets f = f <$> get
 
 exec :: StateM a -> S.State -> S.State
 exec = M.execState
@@ -32,16 +51,10 @@ exec = M.execState
 eval :: StateM a -> S.State -> a
 eval = M.evalState
 
-gets :: (S.State -> s) -> StateM s
-gets = M.gets
-
-modify :: (S.State -> S.State) -> StateM ()
-modify = M.modify
-
-newPerson :: StateM P.Person
+newPerson :: (MonadState m) => m P.Person
 newPerson = P.newWithId <$> newPersonId
 
-newPersonId :: StateM P.Id
+newPersonId :: (MonadState m) => m P.Id
 newPersonId = do
     id <- gets S.nextPersonId
     modify S.incrementPersonId
@@ -53,20 +66,20 @@ and add it to the game.
 
 Return the person.
 -}
-newPersonWith :: (P.Person -> P.Person) -> StateM P.Person
+newPersonWith :: (MonadState m) => (P.Person -> P.Person) -> m P.Person
 newPersonWith init = do
     p <- init <$> newPerson
     addPerson p
     return p
 
-addPerson :: P.Person -> StateM ()
+addPerson :: (MonadState m) => P.Person -> m ()
 addPerson p = do
     modify $ \ s -> s { S.people = p : S.people s }
 
-today :: StateM D.Date
+today :: (MonadState m) => m D.Date
 today = gets S.today
 
-marry :: P.Person -> P.Person -> StateM S.Marriage
+marry :: (MonadState m) => P.Person -> P.Person -> m S.Marriage
 marry p q = do
     t <- today
     let m = S.MkMarriage t Nothing (P.id p) (P.id q)
