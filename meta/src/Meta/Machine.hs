d6dd5df600e8396f6cb312eb17ab051f0179069e
Erik Dominikus <software@spacetimecat.com>
2018-07-07 07:24:59 +0700

    Sketch Machine

--- meta/src/Meta/Machine.hs
+++ meta/src/Meta/Machine.hs
@@ -2,14 +2,25 @@ module Meta.Machine (
     Machine
     , empty
     , localhost
-    , host
-    , local
-    , public
-    , Address
+    -- * IP address
+    , Addr
+    , Subnet
+    , Ip
+    , Tcp
+    -- ** Address constructors
+    , self
+    , neigh
+    , world
 ) where
 
+import qualified Meta.Prelude as P
+
+import qualified Data.Word as W
+
+import qualified Meta.List as L
+
 data Machine = MkMachine {
-        _addresses :: [Address]
+        _addresses :: [Addr ()]
     } deriving (Read, Show)
 
 empty :: Machine
@@ -19,27 +30,62 @@ empty = MkMachine {
 
 localhost :: Machine
 localhost = empty {
-        _addresses = [host "127.0.0.1"]
+        _addresses = [self none (ip_ "127.0.0.1") ()]
     }
 
+ip :: String -> Maybe Ip
+ip str = do
+    [Just a, Just b, Just c, Just d] <- return $ map read_oct str_octs
+    return $ MkIp a b c d
+    where
+        read_oct :: String -> Maybe W.Word8
+        read_oct = P.readMaybe
+        str_octs = L.splitOn "." str
+
+ip_ :: String -> Ip
+ip_ str = case ip str of
+    Nothing -> error $ "Invalid IP address string: " ++ str
+    Just x -> x
+
+none :: Addr a
+none = None
+
 -- | Address reachable only from the machine.
-host :: String -> Address
-host = MkAddress Host
+self :: Addr () -> Ip -> a -> Addr a
+self mach = MkAddr (Self mach)
 
--- | address reachable from the machine's local area networks.
-local :: String -> Address
-local = MkAddress Local
+-- | Address reachable from the machine's neighborhoods (local area networks).
+neigh :: Subnet -> Ip -> a -> Addr a
+neigh sub = MkAddr (Neigh sub)
 
--- | address reachable from the Internet.
-public :: String -> Address
-public = MkAddress Public
+-- | Address reachable from the Internet.
+world :: Ip -> a -> Addr a
+world = MkAddr World
 
-data Address
-    = MkAddress Scope String
+data Ip = MkIp W.Word8 W.Word8 W.Word8 W.Word8
     deriving (Read, Show)
 
-data Scope
-    = Host
-    | Local
-    | Public
+type Tcp = W.Word16
+
+{- |
+@a@ represents a protocol on the Internet Protocol (IP).
+
+Let @a = '()'@ for no protocol.
+-}
+data Addr a
+    = None
+    | MkAddr Pov Ip a
+    deriving (Read, Show)
+
+-- | Point of view.
+data Pov
+    = Self (Addr ())
+    | Neigh Subnet
+    | World
+    deriving (Read, Show)
+
+-- | Number of bits in host address.
+type Slash = Int
+
+data Subnet = MkSubnet Ip Slash
     deriving (Read, Show)
