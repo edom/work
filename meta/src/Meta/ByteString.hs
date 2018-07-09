306444dc662bd7089d0cacce26968e42b1d1d94b
Erik Dominikus <software@spacetimecat.com>
2018-07-10 00:38:37 +0700

    Polyfill ByteString fromStrict and toStrict for GHC 7.4 base 4.5

--- meta/src/Meta/ByteString.hs
+++ meta/src/Meta/ByteString.hs
@@ -1,19 +1,42 @@
+{-# LANGUAGE CPP #-}
+
 module Meta.ByteString (
     B.ByteString
     , LazyByteString
     -- * Conversion
-    , BL.toStrict
-    , BL.fromStrict
+    -- $convert
+    , toStrict
+    , fromStrict
     -- * IO
     , C_putStr(..)
 ) where
 
 import Prelude hiding (putStr)
 
+import Data.ByteString (ByteString)
+
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 
 type LazyByteString = BL.ByteString
+
+{- $convert
+These functions are polyfilled on @bytestring <= 0.9@.
+We don't know how efficient these polyfills are.
+
+On @bytestring >= 0.10@, these functions are aliases of @toStrict@ and @fromStrict@ of "Data.ByteString.Lazy".
+-}
+
+toStrict :: LazyByteString -> ByteString
+fromStrict :: ByteString -> LazyByteString
+
+#if MIN_VERSION_bytestring(0,10,0)
+toStrict = BL.toStrict
+fromStrict = BL.fromStrict
+#else
+toStrict lbs = B.concat $ BL.toChunks lbs
+fromStrict bs = BL.fromChunks [bs]
+#endif
 
 {- |
 We should generalize this from IO to MonadIO.
