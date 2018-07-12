4eece0f46fe0e18bbdadf6a103719c2f73cae23f
Erik Dominikus <software@spacetimecat.com>
2018-07-12 16:35:55 +0700

    Add Learn

---
+++ meta/src/Meta/Learn.hs
@@ -0,0 +1,15 @@
+{- |
+* HLearn
+
+    * <https://hackage.haskell.org/packages/search?terms=hlearn>
+
+    * <https://github.com/mikeizbicki/hlearn>
+
+* Datasets
+
+    * <https://en.wikipedia.org/wiki/Data_set>
+
+    * <https://archive.ics.uci.edu/ml/index.php>
+-}
+module Meta.Learn (
+) where
