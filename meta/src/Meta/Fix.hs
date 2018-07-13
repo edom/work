fa92c00c74c277f4ad5744e524a35969b8d1c487
Erik Dominikus <software@spacetimecat.com>
2018-07-14 04:16:31 +0700

    Fix: Add Inject

--- meta/src/Meta/Fix.hs
+++ meta/src/Meta/Fix.hs
@@ -1,5 +1,16 @@
 module Meta.Fix (
     Fix(..)
+    , Inject(..)
 ) where
 
+-- | Fix :: (* -> *) -> *
 newtype Fix f = In { out :: f (Fix f) }
+
+class Inject a b where
+    inject :: a -> b
+
+instance Inject (f (Fix f)) (Fix f) where
+    inject =  In
+
+instance Inject (Fix f) (f (Fix f)) where
+    inject =  out
