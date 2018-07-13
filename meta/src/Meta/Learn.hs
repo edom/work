07f7d457741f6c7acb2745e981ce28ae53b12d8d
Erik Dominikus <software@spacetimecat.com>
2018-07-14 04:37:09 +0700

    Learn: Add notes

--- meta/src/Meta/Learn.hs
+++ meta/src/Meta/Learn.hs
@@ -1,4 +1,26 @@
 {- |
+* How do we do deep learning?
+
+    * <https://github.com/Gabriel439/post-rfc/blob/master/sotu.md#machine-learning>
+
+    * <https://hackage.haskell.org/packages/search?terms=deep+learning>
+
+    * <https://hackage.haskell.org/package/Learning>
+
+    * <https://hackage.haskell.org/package/grenade>
+
+    * <https://hackage.haskell.org/package/haskell-ml>
+
+    * <https://hackage.haskell.org/package/mltool>
+
+    * <https://hackage.haskell.org/package/learn>
+
+    * <https://hackage.haskell.org/package/deeplearning-hs>
+
+    * Bindings
+
+        * <https://hackage.haskell.org/package/tensorflow>
+
 * HLearn
 
     * <https://hackage.haskell.org/packages/search?terms=hlearn>
