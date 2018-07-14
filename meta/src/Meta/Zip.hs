fcb409a6d912b402856b4c1e2c1574ead15428bf
Erik Dominikus <software@spacetimecat.com>
2018-07-14 19:43:44 +0700

    Zip: Document

--- meta/src/Meta/Zip.hs
+++ meta/src/Meta/Zip.hs
@@ -4,14 +4,22 @@
 
 {- |
 This module wraps the @zip-archive@ package.
+
+Make sure that you use @binary >= 0.7@.
+See note in 'Z.toArchiveOrFail'.
 -}
 module Meta.Zip (
+    -- * Reading archives that are already in memory
     Archive
+    , to_archive
+    -- * Obtaining entries
     , get_entries
     , Entry
-    , decompress_or_error
     , get_relative_path
-    , Slurp(..)
+    -- * Decompressing entries
+    , decompress_or_error
+    -- * Experimental
+    , print_entries_recursive
 ) where
 
 import qualified Control.Monad.IO.Class as MI
@@ -19,29 +27,72 @@ import qualified Meta.ByteString as B
 import qualified Meta.Prelude as P
 import qualified Codec.Archive.Zip as Z
 
--- | See 'Z.Archive'.
+{- |
+An archive is a list of 'Entry's.
+
+See 'Z.Archive' in the "Codec.Archive.Zip" module in the @zip-archive@ package.
+-}
 type Archive = Z.Archive
 
+class To_archive a where
+    {- |
+Read the thing to archive.
+
+The parameter @a@ can be 'P.LazyByteString' or 'P.ByteString'.
+    -}
+    to_archive :: (Monad m) => a -> m Archive
+
+instance To_archive P.LazyByteString where
+    to_archive = either fail return . Z.toArchiveOrFail
+
+instance To_archive P.ByteString where
+    to_archive = to_archive . B.fromStrict
+
+-- | This is 'Z.zEntries'.
 get_entries :: Archive -> [Entry]
 get_entries = Z.zEntries
 
+{- |
+This is 'Z.Entry'.
+
+An entry is not yet decompressed.
+Use 'decompress_or_error' to decompress it.
+-}
 type Entry = Z.Entry
 
 -- | This is a partial function.
+-- Unfortunately @zip-archive@ doesn't provide a total function.
 decompress_or_error :: Entry -> B.ByteString
 decompress_or_error = B.toStrict . Z.fromEntry
 
+-- | This is 'Z.eRelativePath'.
 get_relative_path :: Entry -> FilePath
 get_relative_path = Z.eRelativePath
 
 {- |
-See note in 'Z.toArchiveOrFail' about @binary >= 0.7@.
+Print each entry in the archive on its own line.
+The file path is prepended to every line.
+
+If an entry name ends with @.zip@ or @.jar@ (ignoring case),
+then the entry is assumed to be an 'Archive',
+and this function recurs to that.
 -}
-class Slurp m a where
-    slurp :: FilePath -> m a
+print_entries_recursive :: FilePath -> Z.Archive -> IO ()
+print_entries_recursive = go
+    where
+        go prefix archive = do
+            forM_ (Z.get_entries archive) $ \ entry -> do
+                let path = Z.get_relative_path entry
+                if is_zip_file path
+                    then do
+                        putStrLn $ prefix ++ "/" ++ path
+                        let dent = Z.decompress_or_error entry
+                        arch <- Z.to_archive dent
+                        go (prefix ++ "/" ++ path) arch
+                    else putStrLn $ prefix ++ "/" ++ path
 
-instance (MI.MonadIO m) => Slurp m Archive where
-    slurp = P.slurp P.>=> P.raise_either . Z.toArchiveOrFail
-
-instance (MI.MonadIO m) => Slurp m Entry where
-    slurp = MI.liftIO . Z.readEntry []
+        is_zip_file :: FilePath -> Bool
+        is_zip_file path =
+            (d `endsWith` ".zip") || (d `endsWith` ".jar")
+            where
+                d = downcase path
